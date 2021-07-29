97f408230ad5abe976d022a29f54bbaefdc3bb14
luiscelanomrb <luiscelano@twowheelstogo.com>
2021-07-29 13:41:50 -0600

    initializing billing-extension package

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import pkg from "../package.json";
 export default async function register(app) {
   await app.registerPlugin({
     label: "Plugin Example",
-    name: "plugin-example",
+    name: "gift-extension",
     version: pkg.version
   });
 }
